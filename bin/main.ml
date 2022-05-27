a36c4902afec083299d0e5510989493f97e99784
antoninhrlt <antoninhrlt1@gmail.com>
2022-05-27 23:56:16 +0200

    Main package using lib/

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello, World!"
+(* This file is part of 'mulsh' *)
+(* Under the MIT License *)
+(* Copyright (c) 2022 Antonin Hérault *)
+
+open Mulsh
+
+let say_hello =
+  Logger.normal "Welcome on mulsh !";
+  Logger.normal "This project is open source";
+  Logger.normal "Contribute at https://github.com/antoninhrlt/mulsh"
+
+let main =
+  say_hello;
+  Shell.run
